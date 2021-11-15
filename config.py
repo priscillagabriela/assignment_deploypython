aba3b1dc290c550593448ab65e9d8977abac743b
unknown <email.com>
2021-11-15 23:52:57 +0700

    Assignment 3

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Create the Connexion application instance
+connex_app = connexion.App(__name__, specification_dir=basedir)
+
+# Get the underlying Flask app instance
+app = connex_app.app
+
+# Configure the SQLAlchemy part of the app instance
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rghukoqobakphj:e63bbabdf29e261591e2d73617439680792d6c2544a65494e303b2815ad60540@ec2-34-198-189-252.compute-1.amazonaws.com:5432/dbccls2cpm94dl'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the SQLAlchemy db instance
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
